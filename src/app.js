8f2ae9d1631cfe61b488011c601fd4e3cfda7847
gowthamrajdev <gowthamrajdev@gmail.com>
2023-11-02 15:12:59 +0530

    port to point 3000

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const customerDetailsRouter = require('./routes/customer-details');
 const orderDetailsRouter = require('./routes/order-details');
 
 const app = express();
-const port = 80;
+const port = 3000;
 
 app.use(session({ secret: 'MYSECRET',saveUninitialized: false, resave: false, cookie: {secure: false} })); 
 
